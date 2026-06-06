fcbeacfed04d8e8ff302e96fd1f3d1b911004721
IZAAK DIAS <izaakdias@MacBookPro.bbrouter>
2026-06-06 16:14:15 -0300

    refactor(mobile): localize auth session action type

--- mobile-app/src/services/canonical/sessionService.js
+++ mobile-app/src/services/canonical/sessionService.js
@@ -1,5 +1,5 @@
-export { USER_SIGN_OUT } from '../../common-local/types';
 export { firebase } from '../../common-local/config/configureFirebase';
 
+export const USER_SIGN_OUT = 'USER_SIGN_OUT';
 export const FirebaseContext = undefined;
 export const FirebaseProvider = undefined;
